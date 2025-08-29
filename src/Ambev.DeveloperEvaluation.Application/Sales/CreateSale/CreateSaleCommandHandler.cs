bc077333a6bafdaf8798c0aaf5bdb7487ab2ff2b
Cristiano Antonio <cristianodonascimento256@gmail.com>
2025-08-29 13:54:20 -0300

    test: update SaleTestData and shared builders for improved Faker usage in integration tests

--- src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleCommandHandler.cs
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleCommandHandler.cs
@@ -5,6 +5,7 @@ using Ambev.DeveloperEvaluation.Domain.ValueObjects;
 using Ambev.DeveloperEvaluation.Domain.Events;
 using AutoMapper;
 using MediatR;
+using Ambev.DeveloperEvaluation.Application.Events;
 
 namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
 
@@ -16,9 +17,9 @@ public class CreateSaleCommandHandler : IRequestHandler<CreateSaleCommand, Creat
     private readonly ISaleRepository _saleRepository;
     private readonly IMapper _mapper;
     private readonly IMediator _mediator;
-    private readonly Ambev.DeveloperEvaluation.Application.Events.IEventDispatcher _eventDispatcher;
+    private readonly IEventDispatcher _eventDispatcher;
 
-    public CreateSaleCommandHandler(ISaleRepository saleRepository, IMapper mapper, IMediator mediator, Ambev.DeveloperEvaluation.Application.Events.IEventDispatcher eventDispatcher)
+    public CreateSaleCommandHandler(ISaleRepository saleRepository, IMapper mapper, IMediator mediator, IEventDispatcher eventDispatcher)
     {
         _saleRepository = saleRepository;
         _mapper = mapper;
@@ -41,7 +42,7 @@ public class CreateSaleCommandHandler : IRequestHandler<CreateSaleCommand, Creat
 
 
     var saleCreatedEvent = new SaleCreatedEvent(sale);
-    var integrationEvent = new Events.SaleCreatedIntegrationEvent(sale);
+    var integrationEvent = new SaleCreatedIntegrationEvent(sale);
     await _mediator.Publish(integrationEvent, cancellationToken);
     await _eventDispatcher.DispatchAsync(saleCreatedEvent, cancellationToken);
 
